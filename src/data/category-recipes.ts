da5155b77b32690ca8665d1074149ed9ae6b6c27
Rido Atmanto <ridoatmanto@gmail.com>
2024-08-11 15:11:20 +0700

    feat: category recipe seeder data

---
+++ src/data/category-recipes.ts
@@ -0,0 +1,12 @@
+export const categoryRecipes = [
+  {
+    id: "cat-recipe-1",
+    recipeId: "recipe-1",
+    categoryId: "cat-1",
+  },
+  {
+    id: "cat-recipe-2",
+    recipeId: "recipe-1",
+    categoryId: "cat-2",
+  },
+];
