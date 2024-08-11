a5ba358fa45c7115bf87f7ebd719711ee6a0baa9
Rido Atmanto <ridoatmanto@gmail.com>
2024-08-11 11:08:56 +0700

    feat: include category name

--- src/routes/recipes.tsx
+++ src/routes/recipes.tsx
@@ -3,98 +3,118 @@ import { prisma } from "../lib/prisma";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 
-const app = new Hono();
+export const app = new Hono();
 
 app.get("/", async (c) => {
-	try {
-		const allRecipie = await prisma.recipes.findMany();
-		return c.json(
-			{
-				success: true,
-				message: "List data Recipes",
-				data: allRecipie,
-			},
-			200
-		);
-	} catch (error) {
-		console.error(`Error getting recipes: ${error}`);
-		return c.json(
-			{ success: false, message: "Failed to fetch recipes" },
-			500
-		);
-	}
+  try {
+    // const getPosts = await prisma.post.findMany({
+    // 	where: {
+    // 	  title: {
+    // 		contains: 'cookies',
+    // 	  },
+    // 	},
+    // 	include: {
+    // 	  author: true, // Return all fields
+    // 	},
+    //   })
+    const allRecipes = await prisma.recipes.findMany({
+      where: {
+        //   title: {
+        // 	contains: 'cookies',
+        //   },
+      },
+      orderBy: { createdAt: "desc" },
+      include: { categoryRecipes: { include: { categories: true } } },
+    });
+    return c.json(
+      {
+        success: true,
+        message: "List data Recipes",
+        data: allRecipes,
+      },
+      200
+    );
+  } catch (error) {
+    console.error(`Error getting recipes: ${error}`);
+    return c.json({ success: false, message: "Failed to fetch recipes" }, 500);
+  }
 });
 
 app.get("/:slug", async (c) => {
-	try {
-		const slugParam = c.req.param("slug");
+  try {
+    const slugParam = c.req.param("slug");
 
-		if (!slugParam) {
-			return c.json({ message: "Recipe slug needed" }, 400);
-		}
+    if (!slugParam) {
+      return c.json({ message: "Recipe slug needed" }, 400);
+    }
 
-		const recipe = await prisma.recipes.findFirst({
-			where: { slug: slugParam },
-		});
+    const recipe = await prisma.recipes.findFirst({
+      where: { slug: slugParam },
+      include: {
+        categoryRecipes: {
+          include: { categories: true },
+        },
+      },
+    });
 
-		if (!recipe) {
-			return c.json({ message: "Recipe doesn't exist!" }, 404);
-		}
+    if (!recipe) {
+      return c.json({ message: "Recipe doesn't exist!" }, 404);
+    }
 
-		return c.json({
-			success: true,
-			message: `Recipe ${recipe.slug}`,
-			data: recipe,
-		});
-	} catch (err: any) {
-		console.log(`Error retrieving recipe: ${err.message}`);
-		return c.json({ message: "Failed to retrieve recipe" }, 500);
-	}
+    return c.json({
+      success: true,
+      message: `Recipe ${recipe.slug}`,
+      data: recipe,
+    });
+  } catch (err: any) {
+    console.log(`Error retrieving recipe: ${err.message}`);
+    return c.json({ message: "Failed to retrieve recipe" }, 500);
+  }
 });
 
 app.post(
-	"/create",
-	zValidator(
-		"json",
-		z.object({
-			recipe: z.string(),
-			description: z.string(),
-			imageURL: z.string().url(),
-			slug: z.string(),
-			ingredients: z.string(),
-			cookingIntructions: z.string(),
-			userId: z.string(),
-		})
-	),
-	async (c) => {
-		const body = c.req.valid("json");
-		try {
-			const newRecipe = await prisma.recipes.create({
-				data: {
-					recipe: body.recipe,
-					description: body.description,
-					imageURL: body.imageURL,
-					slug: body.slug,
-					ingredients: body.ingredients,
-					cookingIntructions: body.cookingIntructions,
-					userId: body.userId,
-				},
-			});
-			return c.json(
-				{
-					success: true,
-					message: "New recipe created successfully",
-					newRecipe: {
-						recipe: newRecipe.recipe,
-					},
-				},
-				201
-			);
-		} catch (error) {
-			console.error(`Error creating recipe: ${error}`);
-			return c.json({ message: "Cannot create recipe." }, 400);
-		}
-	}
+  "/create",
+  zValidator(
+    "json",
+    z.object({
+      recipe: z.string(),
+      description: z.string(),
+      imageURL: z.string().url(),
+      slug: z.string(),
+      ingredients: z.string(),
+      cookingIntructions: z.string(),
+      userId: z.string(),
+    })
+  ),
+  async (c) => {
+    const body = c.req.valid("json");
+    try {
+      const newRecipe = await prisma.recipes.create({
+        data: {
+          recipe: body.recipe,
+          description: body.description,
+          imageURL: body.imageURL,
+          slug: body.slug,
+          ingredients: body.ingredients,
+          cookingIntructions: body.cookingIntructions,
+          userId: body.userId,
+        },
+      });
+      return c.json(
+        {
+          success: true,
+          message: "New recipe created successfully",
+          newRecipe: {
+            recipe: newRecipe.recipe,
+          },
+        },
+        201
+      );
+    } catch (error) {
+      console.error(`Error creating recipe: ${error}`);
+      return c.json({ message: "Cannot create recipe." }, 400);
+    }
+  }
 );
 
-export const recipesRoute = app;
+export default app;
