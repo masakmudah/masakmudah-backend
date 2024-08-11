411569e86e061ae0e4b4cccdc86cd816adab1b6e
Rido Atmanto <ridoatmanto@gmail.com>
2024-08-11 18:19:03 +0700

    feat: add duration on seed

--- src/data/recipes.ts
+++ src/data/recipes.ts
@@ -3,104 +3,100 @@ export const recipes = [
     id: "recipe-1",
     recipe: "Nasi Goreng",
     description: "Resep nasi goreng spesial.",
+    duration: "30 menit",
     imageURL:
       "https://img-global.cpcdn.com/recipes/9b66747d60cbff8e/640x640sq70/photo.webp",
     slug: "nasi-goreng",
-    cookingIntructions: "Goreng nasi dengan kecap dan telur.",
     userId: "user-1",
   },
   {
     id: "recipe-2",
     recipe: "Sate Ayam",
     description: "Resep sate ayam dengan bumbu kacang.",
+    duration: "30 menit",
     imageURL:
       "https://img-global.cpcdn.com/recipes/8aaae9483e227043/640x640sq70/photo.webp",
     slug: "sate-ayam",
-    cookingIntructions:
-      "Tusuk ayam, panggang, dan sajikan dengan bumbu kacang.",
     userId: "user-2",
   },
   {
     id: "recipe-3",
     recipe: "Rendang Daging",
     description: "Resep rendang daging khas Padang.",
+    duration: "2 jam",
     imageURL:
       "https://img-global.cpcdn.com/recipes/71371aa56173f543/640x640sq70/photo.webp",
     slug: "rendang-daging",
-    cookingIntructions: "Masak daging dengan bumbu rendang hingga empuk.",
     userId: "user-1",
   },
   {
     id: "recipe-4",
     recipe: "Gado-Gado",
     description: "Resep gado-gado sayur dengan saus kacang.",
+    duration: "20 menit",
     imageURL:
       "https://img-global.cpcdn.com/recipes/609289176a0bd330/640x640sq70/photo.webp",
     slug: "gado-gado",
-    cookingIntructions: "Campur sayuran dengan saus kacang dan sajikan.",
     userId: "user-2",
   },
   {
     id: "recipe-5",
     recipe: "Bakso",
     description: "Resep bakso daging sapi dengan kuah kaldu.",
+    duration: "60 menit",
     imageURL:
       "https://img-global.cpcdn.com/recipes/410f986bf2d274d7/640x640sq70/photo.webp",
     slug: "bakso",
-    cookingIntructions: "Bentuk bakso, rebus, dan sajikan dengan kuah kaldu.",
     userId: "user-1",
   },
   {
     id: "recipe-6",
     recipe: "Mie Goreng",
     description: "Resep mie goreng dengan telur dan sayuran.",
+    duration: "10 menit",
     imageURL:
       "https://img-global.cpcdn.com/recipes/4b79019187c13f75/640x640sq70/photo.webp",
     slug: "mie-goreng",
-    cookingIntructions: "Tumis mie dengan sayuran dan telur, tambahkan kecap.",
     userId: "user-2",
   },
   {
     id: "recipe-7",
     recipe: "Soto Ayam",
     description: "Resep soto ayam kuah kuning.",
+    duration: "60 menit",
     imageURL:
       "https://img-global.cpcdn.com/recipes/3b54f33e81fc64c8/640x640sq70/photo.webp",
     slug: "soto-ayam",
-    cookingIntructions: "Rebus ayam dengan bumbu soto, sajikan dengan nasi.",
     userId: "user-1",
   },
   {
     id: "recipe-8",
     recipe: "Pecel Lele",
     description: "Resep pecel lele dengan sambal terasi.",
+    duration: "30 menit",
     imageURL:
       "https://img-global.cpcdn.com/recipes/457e8176ae1d550c/640x640sq70/photo.webp",
     slug: "pecel-lele",
-    cookingIntructions:
-      "Goreng lele dan sajikan dengan sambal terasi dan lalapan.",
     userId: "user-2",
   },
   {
     id: "recipe-9",
     recipe: "Gudeg Jogja",
     description: "Resep gudeg khas Jogja dengan nangka muda.",
+    duration: "60 menit",
     imageURL:
       "https://img-global.cpcdn.com/recipes/3dfd2e7d0cda4552/640x640sq70/photo.webp",
     slug: "gudeg-jogja",
-    cookingIntructions:
-      "Masak nangka dengan bumbu gudeg dan santan hingga empuk.",
     userId: "user-1",
   },
   {
     id: "recipe-10",
     recipe: "Nasi Uduk",
     description: "Resep nasi uduk dengan sambal kacang.",
+    duration: "60 menit",
     imageURL:
       "https://img-global.cpcdn.com/recipes/ad009aa4e8049ce0/640x640sq70/photo.webp",
     slug: "nasi-uduk",
-    cookingIntructions:
-      "Masak nasi dengan santan dan bumbu, sajikan dengan sambal kacang.",
     userId: "user-2",
   },
 ];
