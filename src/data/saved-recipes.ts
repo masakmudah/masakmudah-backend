48763d4abf4ece8bb4adcae78754a5e7f2c1a243
Rido Atmanto <ridoatmanto@gmail.com>
2024-08-11 18:20:05 +0700

    feat: update data seeder

---
+++ src/data/saved-recipes.ts
@@ -0,0 +1,12 @@
+export const savedRecipes = [
+  {
+    id: "saved-recipe-1",
+    recipeId: "recipe-3",
+    userId: "user-1",
+  },
+  {
+    id: "saved-recipe-2",
+    recipeId: "recipe-4",
+    userId: "user-2",
+  },
+];
