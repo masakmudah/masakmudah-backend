d70d80e5617d2208807d49a696a63c6b57c30b49
Cahyono Muslim Sidiq <cdq.cahyono@gmail.com>
2024-08-12 15:50:49 +0700

    feat: getall categories

--- src/routes/categories.tsx
+++ src/routes/categories.tsx
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import {prisma} from "../lib/prisma";
+import { prisma } from "../lib/prisma";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 import { checkUserToken } from "../midleware/cekUserToken";
@@ -9,66 +9,64 @@ import { HonoApp } from "..";
 const app = new Hono<HonoApp>();
 
 app.get("/", async (c) => {
-	try {
-		return c.json(
-			{
-				success: true,
-				message: "List data Categories",
-				data: [
-					{
-						id: "cat-1",
-						category: "Ayam",
-						createdAt: "2024-08-10T11:02:49.177Z",
-						updatedAt: "2024-08-10T11:02:49.177Z",
-					},
-					{
-						id: "cat-2",
-						category: "Sayuran",
-						createdAt: "2024-08-10T11:02:49.177Z",
-						updatedAt: "2024-08-10T11:02:49.177Z",
-					},
-					{
-						id: "cat-3",
-						category: "Sapi",
-						createdAt: "2024-08-10T11:02:49.177Z",
-						updatedAt: "2024-08-10T11:02:49.177Z",
-					},
-					{
-						id: "cat-4",
-						category: "Seafood",
-						createdAt: "2024-08-10T11:02:49.177Z",
-						updatedAt: "2024-08-10T11:02:49.177Z",
-					},
-				],
-			},
-			200
-		);
-	} catch (error) {
-		console.error(`Error getting categories: ${error}`);
-		return c.json(
-			{ success: false, message: "Failed to fetch categories" },
-			500
-		);
-	}
+  try {
+    return c.json(
+      {
+        success: true,
+        message: "List data Categories",
+        data: [
+          {
+            id: "cat-1",
+            category: "Ayam",
+            createdAt: "2024-08-10T11:02:49.177Z",
+            updatedAt: "2024-08-10T11:02:49.177Z",
+          },
+          {
+            id: "cat-2",
+            category: "Sayuran",
+            createdAt: "2024-08-10T11:02:49.177Z",
+            updatedAt: "2024-08-10T11:02:49.177Z",
+          },
+          {
+            id: "cat-3",
+            category: "Sapi",
+            createdAt: "2024-08-10T11:02:49.177Z",
+            updatedAt: "2024-08-10T11:02:49.177Z",
+          },
+          {
+            id: "cat-4",
+            category: "Seafood",
+            createdAt: "2024-08-10T11:02:49.177Z",
+            updatedAt: "2024-08-10T11:02:49.177Z",
+          },
+        ],
+      },
+      200
+    );
+  } catch (error) {
+    console.error(`Error getting categories: ${error}`);
+    return c.json(
+      { success: false, message: "Failed to fetch categories" },
+      500
+    );
+  }
 });
 
-
 app.get("/allCategories", checkUserToken(), async (c) => {
-	try {
-		const categories = await prisma.categories.findMany({
-  include: {
-    categoryRecipes: true,
-  },
-		})
-		
-		return c.json(categories)
-	} catch (error) {
-		console.error("Data Categories notfound  ");
-		return c.json({ message: "Cannot create Category." }, 400);
-	}
-	
+  try {
+    const categories = await prisma.categories.findMany({
+      select: {
+        category: true,
+        CategoryRecipes: true,
+      },
+    });
 
-})
+    return c.json(categories);
+  } catch (error) {
+    console.error("Failed to retrieve categories:", error);
+    return c.json({ message: "Cannot retrieve categories." }, 400);
+  }
+});
 
 app.post(
   "/create",
@@ -83,13 +81,12 @@ app.post(
     const user = c.get("user");
     const body = c.req.valid("json");
     try {
-			const newCategory = await prisma.categories.create({
-				
+      const newCategory = await prisma.categories.create({
         data: {
           category: body.category,
-				},
-			});
-			
+        },
+      });
+
       return c.json(
         {
           success: true,
@@ -107,32 +104,26 @@ app.post(
   }
 );
 
-
 app.put("/:id", checkUserToken(), async (c) => {
-	
-try {
-	const id = c.req.param("id");
-	const body = await c.req.json();
-	
-	if (!id) {
-			return c.json({ message: `Categori not found`, Status: 404 });
-	}
-	
-		const newCategory = await prisma.categories.update({
-			where: { id },
-			data: {
-				category: String(body.category),
-			},
-		});
-	
-		return c.json(newCategory);
-	
-	
-} catch (error) {
-	
-	console.error(`Error Category : ${error}`);
-}
+  try {
+    const id = c.req.param("id");
+    const body = await c.req.json();
 
-})
+    if (!id) {
+      return c.json({ message: `Categori not found`, Status: 404 });
+    }
+
+    const newCategory = await prisma.categories.update({
+      where: { id },
+      data: {
+        category: String(body.category),
+      },
+    });
+
+    return c.json(newCategory);
+  } catch (error) {
+    console.error(`Error Category : ${error}`);
+  }
+});
 
 export const categories = app;
