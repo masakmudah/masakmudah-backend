3e21181d4001f28c57c9602340f01f8562843b94
Rido Atmanto <ridoatmanto@gmail.com>
2024-08-17 18:06:34 +0700

    feat: add swagger on route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { Hono } from "hono";
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import { cors } from "hono/cors";
 
 import { registerRoute } from "./routes/register";
-import { userRoute } from "./routes/user";
+import { userRoute } from "./users/route";
 import { loginRoute } from "./routes/login";
 import { recipesRoute } from "./routes/recipes";
 import { categories } from "./routes/categories";
@@ -19,9 +20,32 @@ type Variables = {
 
 export type HonoApp = { Bindings: Bindings; Variables: Variables };
 
-const app = new Hono();
+const app = new OpenAPIHono({ strict: false });
 
 app.use("*", cors());
+
+// OPEN API
+app.doc31("/api-spec", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "MasakMudah REST API",
+    description:
+      "MasakMudah provides access to a large collection of recipe.\n\nIt allows users to search for recipe, retrieve recipe details, and publish own recipe also saving another user recipe.",
+  },
+});
+
+// SECURITY SCHEMES
+app.openAPIRegistry.registerComponent(
+  "securitySchemes",
+  "AuthorizationBearer",
+  {
+    type: "http",
+    scheme: "bearer",
+    bearerFormat: "JWT",
+  }
+);
+
 app.get("/", (c) => {
   return c.json({
     Message: "API Masak Mudah",
@@ -30,6 +54,7 @@ app.get("/", (c) => {
     usersURL: "/users",
     recipesURL: "/recipes",
     categoriesURL: "/categories",
+    swaggerURL: "/ui",
   });
 });
 
@@ -38,5 +63,8 @@ app.route("/auth/register", registerRoute);
 app.route("/users", userRoute);
 app.route("/recipes", recipesRoute);
 app.route("/categories", categories);
+
+// SWAGGER UI
+app.get("/ui", swaggerUI({ url: "/api-spec" }));
 
 export default app;
