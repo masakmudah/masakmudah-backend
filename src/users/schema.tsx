3560021a682ce68304ef962c5da87542024851bb
Masakmudah Admin <masakmudah.com@gmail.com>
2024-08-18 19:34:53 +0700

    Update schema.tsx

--- src/users/schema.tsx
+++ src/users/schema.tsx
@@ -4,6 +4,11 @@ export const QueryUserSchema = z.object({
   search: z.string().optional(),
 });
 
+export const UserSchema = z.object({
+  fullname: z.string().optional(),
+  email: z.string().optional(),
+});
+
 export const DetailUserSchema = z.object({
   id: z.string().min(1).openapi({ example: "uN1QueU53rID" }),
 });
