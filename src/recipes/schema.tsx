a5b38864b2a6c6967f5917f1c93b30e056331700
Rido Atmanto <ridoatmanto@gmail.com>
2024-08-19 20:17:25 +0700

    feat: remove default params swagger

--- src/recipes/schema.tsx
+++ src/recipes/schema.tsx
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const QueryRecipeSchema = z.object({
-  search: z.string().optional().openapi({ example: "go" }),
+  search: z.string().optional(),
 });
 
 export const SearchByCategorySchema = z.object({
-  categoryId: z.string().openapi({ example: "uuflc5ng06nqxge861rtpq4s" }),
+  categoryId: z.string(),
 });
 
 export const DetailRecipeSchema = z.object({
-  slug: z.string().min(1).openapi({ example: "nasi-goreng" }),
+  slug: z.string().min(1),
 });
