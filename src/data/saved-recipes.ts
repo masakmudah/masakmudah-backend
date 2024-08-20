a50db8afe4800b553aec96c9d235a1f27112b78c
Masakmudah Admin <masakmudah.com@gmail.com>
2024-08-20 13:31:52 +0700

    feat: move data seed into prisma folder

---
+++ src/data/saved-recipes.ts
@@ -0,0 +1,12 @@
+export const savedRecipes = [
+  {
+    id: "rm5sdfjddwricw8gjnewww4h",
+    recipeId: "xlltcvjp1n2ehpddgf0vbajd",
+    userId: "f58ch2o6z749s7wj4cwbc7jg",
+  },
+  {
+    id: "ick01hktoaaoklv57hok3alo",
+    recipeId: "mqc8by4941kbxipuldyu1ymz",
+    userId: "spfilbroj5xoa0aoghyh5t40",
+  },
+];
---
+++ src/seed.ts
@@ -0,0 +1,119 @@
+import { categoryRecipes } from "./data/category-recipes";
+import { categories } from "./data/categories";
+import { recipes } from "./data/recipes";
+import { ingredients } from "./data/ingredients";
+import { instructions } from "./data/instructions";
+import { savedRecipes } from "./data/saved-recipes";
+import { users } from "./data/users";
+import { prisma } from "../src/lib/prisma";
+import { hashPassword } from "../src/lib/password";
+
+async function seed() {
+  for (let user of users) {
+    const newUser = await prisma.user.upsert({
+      where: {
+        id: user.id,
+      },
+      update: {
+        email: user.email,
+        username: user.username,
+        fullname: user.fullname,
+      },
+      create: {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        fullname: user.fullname,
+        password: {
+          create: {
+            hash: await hashPassword(user.password),
+          },
+        },
+      },
+    });
+
+    console.log(`User with id ${newUser.id} created`);
+  }
+  for (let recipe of recipes) {
+    const newRecipesSeed = await prisma.recipe.upsert({
+      where: {
+        slug: recipe.slug, // Menggunakan slug sebagai nilai unik
+      },
+      update: recipe,
+      create: recipe,
+    });
+    console.log(`Recipe : ${newRecipesSeed.slug}`);
+  }
+
+  for (let category of categories) {
+    const newCategoriesSeed = await prisma.category.upsert({
+      where: {
+        id: category.id, // Menggunakan id sebagai nilai unik
+      },
+      update: category,
+      create: category,
+    });
+    console.log(`Category : ${newCategoriesSeed.category}`);
+  }
+
+  for (let catRecipe of categoryRecipes) {
+    const newCatRecipesSeed = await prisma.categoryRecipe.upsert({
+      where: {
+        id: catRecipe.id, // Menggunakan id sebagai nilai unik
+      },
+      update: catRecipe,
+      create: catRecipe,
+    });
+    console.log(
+      `Category Recipe : ${newCatRecipesSeed.categoryId} for ${newCatRecipesSeed.recipeId}`
+    );
+  }
+
+  for (let ingredient of ingredients) {
+    const newIngredientSeed = await prisma.ingredient.upsert({
+      where: {
+        id: ingredient.id, // Menggunakan id sebagai nilai unik
+      },
+      update: ingredient,
+      create: ingredient,
+    });
+    console.log(
+      `New ingredient : ${ingredient.quantity} ${ingredient.measurement} ${ingredient.name} `
+    );
+  }
+
+  for (let instruction of instructions) {
+    const newInstructionSeed = await prisma.instruction.upsert({
+      where: {
+        id: instruction.id, // Menggunakan id sebagai nilai unik
+      },
+      update: instruction,
+      create: instruction,
+    });
+    console.log(
+      `New instruction : ${instruction.recipeId} ${instruction.text}`
+    );
+  }
+
+  for (let savedRecipe of savedRecipes) {
+    const newSavedRecipeSeed = await prisma.savedRecipe.upsert({
+      where: {
+        id: savedRecipe.id, // Menggunakan id sebagai nilai unik
+      },
+      update: savedRecipe,
+      create: savedRecipe,
+    });
+    console.log(
+      `Category Recipe : ${newSavedRecipeSeed.recipeId} for ${newSavedRecipeSeed.userId}`
+    );
+  }
+}
+
+seed()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
