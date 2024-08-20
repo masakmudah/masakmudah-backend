e4291651d0d10f209db8cafa4f29b81f2db30ff2
Masakmudah Admin <masakmudah.com@gmail.com>
2024-08-20 14:41:08 +0700

    feat: update structure db data

--- prisma/data/categories.ts
+++ prisma/data/categories.ts
@@ -1,18 +1,22 @@
 export const categories = [
   {
     id: "uuflc5ng06nqxge861rtpq4s",
-    category: "Ayam",
+    slug: "ayam",
+    name: "Ayam",
   },
   {
     id: "p6yns61e0huccchmlmlunfru",
-    category: "Sayuran",
+    slug: "sayuran",
+    name: "Sayuran",
   },
   {
     id: "yr5e4f13eiojzi0vpd8yth2w",
-    category: "Sapi",
+    slug: "sapi",
+    name: "Sapi",
   },
   {
     id: "br468pj8zcng7utn2pyhzuta",
-    category: "Seafood",
+    slug: "seafood",
+    name: "Seafood",
   },
 ];
