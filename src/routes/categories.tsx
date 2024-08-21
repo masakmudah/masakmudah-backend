bc801cd308565efcff37c2ba297bfb56062e59cd
Masakmudah Admin <masakmudah.com@gmail.com>
2024-08-21 23:05:05 +0700

    Update categories.tsx

--- src/routes/categories.tsx
+++ src/routes/categories.tsx
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { prisma } from "../lib/prisma";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
-import { checkUserToken } from "../midleware/check-user-token";
+import { checkUserToken } from "../middleware/check-user-token";
 
 import { HonoApp } from "..";
 
